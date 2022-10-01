c54be391beb15acb02073eb910517e2db6b8f84a
hito-kotaro <tohi.work@gmail.com>
2022-10-02 07:48:52 +0900

    findAll実装

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Users } from 'src/entity/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(Users) private usersRepository: Repository<Users>,
+  ) {}
+
+  findAll(): Promise<Users[]> {
+    return this.usersRepository.find();
+  }
+}
